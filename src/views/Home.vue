6c25c09473cd89e3c7974035ceadd1b05a0115dd
azamjonbro <azamjondasturchi11@gmail.com>
2024-06-01 11:38:30 +0500

    added package to github

---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+   <div>
+      <select>
+        <option >salom1</option>
+        <option >salom1</option>
+        <option >salom1</option>
+        <option >salom1</option>
+        <option >salom1</option>
+      </select>
+   </div>
+</template>
